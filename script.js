7c6b6dba1e469816dcf6a89ddea3e2596e11683f
Utsav Kashichhwa <utsavk0112@gmail.com>
2024-01-29 17:49:54 +1100

    JS + Fancy Background

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-fetch("https://tech-club-api.onrender.com/honour-roll-data").then((response) =>
-    response.json()
-);
+const boardsContainter = document.querySelector(".boards-container");
+
+fetch("https://tech-club-api.onrender.com/honour-roll-data")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        data.forEach((board_data) => {
+            const board = document.createElement("div");
+            board.classList.add("board");
+
+            board.innerHTML = `
+                <h2 class="year">${board_data.year}</h2>
+                <div class="people">
+                    ${board_data.people
+                        .map((person) => {
+                            return `
+                            <div class="person-info">
+                                <div class="name">${person.name}, ${person.class}</div>
+                                <div class="title"
+                                >${person.title}</div>
+                            </div>
+
+                            `;
+                        })
+                        .join("")}
+                </div>
+                `;
+            boardsContainter.appendChild(board);
+        });
+    });
